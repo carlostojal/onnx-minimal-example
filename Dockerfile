8166f0e3660bbe65b8e992b41f2c1a0472d13056
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-03-20 17:47:59 +0000

    clean up network introspection code

--- Dockerfile
+++ Dockerfile
@@ -13,4 +13,4 @@ RUN cmake ..
 RUN make -j4
 
 # run the app
-CMD ["./onnx_minimal_example", "path/to/image.jpg", "path/to/model.onnx"]
+CMD ["./onnx_minimal_example", "path/to/image.jpg", "../model/damoyolo_fsoco.onnx"]
--- src/main.cpp
+++ src/main.cpp
@@ -20,50 +20,63 @@ int main(int argc, char* argv[]) {
 	std::cout << "Number of input nodes: " << num_input_nodes << std::endl;
 	std::cout << "Number of output nodes: " << num_output_nodes << std::endl;
 
-	// print the input and output node shapes
-	std::cout << "Input node shapes:" << std::endl;
-	for (size_t i = 0; i < num_input_nodes; i++) {
-		Ort::TypeInfo type_info = session.GetInputTypeInfo(i);
-		auto tensor_info = type_info.GetTensorTypeAndShapeInfo();
-		std::vector<int64_t> input_shape = tensor_info.GetShape();
-		std::cout << "Input node " << i << " shape: ";
-		for (size_t j = 0; j < input_shape.size(); j++) {
-			std::cout << input_shape[j] << " ";
+	// get the image tensor shape
+	std::cout << "Image tensor shape: ";
+	Ort::TypeInfo image_type_info = session.GetInputTypeInfo(0);
+	auto image_tensor_info = image_type_info.GetTensorTypeAndShapeInfo();
+	std::vector<int64_t> image_shape = image_tensor_info.GetShape();
+	std::cout << "(";
+	for (size_t i = 0; i < image_shape.size(); i++) {
+		std::cout << image_shape[i] << " ";
+	}
+	std::cout << ")" << std::endl;
+
+	// get the probability tensor shape and name
+	std::cout << "Probability tensor shape: ";
+	Ort::TypeInfo prob_type_info = session.GetOutputTypeInfo(0);
+	auto prob_tensor_info = prob_type_info.GetTensorTypeAndShapeInfo();
+	std::vector<int64_t> prob_shape = prob_tensor_info.GetShape();
+	std::cout << "(";
+	for (size_t i = 0; i < prob_shape.size(); i++) {
+		std::cout << prob_shape[i] << " ";
+	}
+	std::cout << ")" << std::endl;
+
+	// get the bounding box tensor shape
+	std::cout << "Bounding box tensor shape: ";
+	Ort::TypeInfo bbox_type_info = session.GetOutputTypeInfo(1);
+	auto bbox_tensor_info = bbox_type_info.GetTensorTypeAndShapeInfo();
+	std::vector<int64_t> bbox_shape = bbox_tensor_info.GetShape();
+	std::cout << "(";
+	for (size_t i = 0; i < bbox_shape.size(); i++) {
+		std::cout << bbox_shape[i] << " ";
+	}
+	std::cout << ")" << std::endl;
+
+	/*
+	// load the input image
+	cv::Mat img = cv::imread(args.img_path, cv::IMREAD_COLOR);
+	if (img.empty()) {
+		std::cerr << "Could not read the image: " << args.img_path << std::endl;
+		return 1;
+	}
+
+	// preprocess the input image
+	cv::Mat img_resized;
+	cv::resize(img, img_resized, cv::Size(image_shape[2], image_shape[3]));
+
+	// convert the input image to a tensor
+	std::vector<float> input_tensor_values;
+	for (int i = 0; i < input_shape[2]; i++) {
+		for (int j = 0; j < input_shape[3]; j++) {
+			cv::Vec3b pixel = img_resized.at<cv::Vec3b>(i, j);
+			input_tensor_values.push_back(pixel[0]);
+			input_tensor_values.push_back(pixel[1]);
+			input_tensor_values.push_back(pixel[2]);
 		}
-		std::cout << std::endl;
 	}
-	std::cout << "Output node shapes:" << std::endl;
-	for (size_t i = 0; i < num_output_nodes; i++) {
-		Ort::TypeInfo type_info = session.GetOutputTypeInfo(i);
-		auto tensor_info = type_info.GetTensorTypeAndShapeInfo();
-		std::vector<int64_t> output_shape = tensor_info.GetShape();
-		std::cout << "Output node " << i << " shape: ";
-		for (size_t j = 0; j < output_shape.size(); j++) {
-			std::cout << output_shape[j] << " ";
-		}
-		std::cout << std::endl;
-	}
-
-	// print the input and output node types
-	std::cout << "Input node types:" << std::endl;
-	for (size_t i = 0; i < num_input_nodes; i++) {
-		Ort::TypeInfo type_info = session.GetInputTypeInfo(i);
-		auto tensor_info = type_info.GetTensorTypeAndShapeInfo();
-		ONNXTensorElementDataType input_type = tensor_info.GetElementType();
-		std::cout << "Input node " << i << " type: " << input_type << std::endl;
-	}
-
-	std::cout << "Output node types:" << std::endl;
-	for (size_t i = 0; i < num_output_nodes; i++) {
-		Ort::TypeInfo type_info = session.GetOutputTypeInfo(i);
-		auto tensor_info = type_info.GetTensorTypeAndShapeInfo();
-		ONNXTensorElementDataType output_type = tensor_info.GetElementType();
-		std::cout << "Output node " << i << " type: " << output_type << std::endl;
-	}
-
-	// TODO: create the input and output tensors
-
-	// TODO: load the input image into the input tensor
+	Ort::Value input_tensor = Ort::Value::CreateTensor<float>(env, input_tensor_values.data(), input_tensor_values.size(), input_shape.data(), input_shape.size());
+	*/
 
 	return 0;
 }
